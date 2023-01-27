77a8502dfbac880bbedf6ceafe46af1b015adc37
O'Neill, Bert <bert.oneill@daera-ni.gov.uk>
2023-01-27 15:29:21 +0000

    adding test

---
+++ DaeraEnd2EndTests/Base Class/PageTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright.MSTest;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DaeraEnd2EndTests.Base_Class
+{
+    [TestClass]
+    public class PageTest : ContextTest
+    {
+        public IPage? Page { get; private set; }
+        [TestInitialize]
+        public async Task PageSetup()
+        {
+            Page = await Context!.NewPageAsync().ConfigureAwait(false);
+        }
+    }
+}
--- DaeraEnd2EndTests/PaymentsScreens.cs
+++ DaeraEnd2EndTests/PaymentsScreens.cs
@@ -1,9 +1,15 @@
 using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
 
 namespace DaeraEnd2EndTests
 {
     [TestClass]
-    public class PaymentsScreens
+    public class PaymentsScreens : PageTest
     {
         private readonly IConfiguration _configuration;
 
@@ -18,37 +24,34 @@ namespace DaeraEnd2EndTests
             return config;
         }
 
-        public PaymentsScreens()
+        [TestMethod]
+        public async Task ShouldOpenPlaywright()
         {
-           
+            //Given I am on https://www.google.com
+            await Page.GotoAsync("https://www.google.com");
+
+            //When I type dotnetcoretutorials.com into the search box
+            await Page.FillAsync("[title='Search']", "dotnetcoretutorials.com");
+
+            //And I press the button with the text "Google Search"
+            await Page.ClickAsync("[value='Google Search'] >> nth=1");
+
+            //Then the first result is domain dotnetcoretutorials.com
+            var firstResult = await Page.Locator("//cite >> nth=0").InnerTextAsync();
+            Assert.AreEqual("https://dotnetcoretutorials.com", firstResult);
         }
 
-        [TestMethod]
+
+    [TestMethod]
+        [Ignore]
         public async Task PaymentSearchHomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
         {
-
-
-
             var config = InitConfiguration();
             var clientId = config["PaymentSettings:URL"];
 
+            await Page.GotoAsync("https://playwright.dev");
+            await Expect(Page.Locator("text=enables reliable end-to-end testing for modern web apps")).ToBeVisibleAsync();
 
-            //await Page.GotoAsync("https://playwright.dev");
-
-            //// Expect a title "to contain" a substring.
-            //await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-            //// create a locator
-            //var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-
-            //// Expect an attribute "to be strictly equal" to the value.
-            //await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-            //// Click the get started link.
-            //await getStarted.ClickAsync();
-
-            //// Expects the URL to contain intro.
-            //await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
         }
 
 
